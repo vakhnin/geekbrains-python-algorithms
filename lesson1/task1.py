1738967d7b29dcc3d9d12e153943931ad8354050
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-04-05 14:31:44 +0300

    Рефакторизация задачи 1

--- lesson1/task1.py
+++ lesson1/task1.py
@@ -1,11 +1,9 @@
 # Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 
-sumDigs = 0
-multDigs = 1
 n = int(input("Введите целое число из 3 цифр:"))
 
-sumDigs += n % 10
-multDigs *= n % 10
+sumDigs = n % 10
+multDigs = n % 10
 n = n // 10
 
 sumDigs += n % 10
