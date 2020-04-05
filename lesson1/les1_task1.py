8c935f151d2f68f6b353d56e20c675eb530088f3
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-04-05 17:31:30 +0300

    Добавление ссылки на блок-схемы

--- lesson1/les1_task1.py
+++ lesson1/les1_task1.py
@@ -1,3 +1,6 @@
+# Ссылка на блок-схему (страницы листаются):
+# https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&page-id=jhGgn7LUSzazaFBRB71W&title=lesson1.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1zM8iK8lKuEx1tTLTTswxOY3cYeJBrRln%26export%3Ddownload
+#
 # Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 
 print("Введите целое число из 3 цифр:")
--- lesson1/les1_task9.py
+++ lesson1/les1_task9.py
@@ -1,3 +1,6 @@
+# Ссылка на блок-схему (страницы листаются):
+# https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&page-id=jhGgn7LUSzazaFBRB71W&title=lesson1.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1zM8iK8lKuEx1tTLTTswxOY3cYeJBrRln%26export%3Ddownload
+#
 # Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
 print("Введите три числа")
 
