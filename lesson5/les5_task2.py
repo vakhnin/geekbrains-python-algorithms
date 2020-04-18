76871a8b49636bfc51b6e01cfcbf9e16faca6287
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-04-18 16:32:36 +0300

    Задача 2: сложение чисел

---
+++ lesson5/les5_task2.py
@@ -0,0 +1,30 @@
+from _collections import deque
+
+table = tuple("0123456789ABCDEF")
+
+a = deque("A2")
+b = deque("C4F")
+c = deque()
+
+if len(a) < len(b):
+    a, b = b, a
+
+print(a)
+print(b)
+
+transfer = 0
+while a:
+    c1 = table.index(a.pop())
+    print(c1)
+    c2 = table.index(b.pop()) if b else 0
+    res_spam = c1 + c2 + transfer
+    if res_spam > len(table):
+        res_spam -= len(table)
+        transfer = 1
+    else:
+        transfer = 0
+    c.appendleft(table[res_spam])
+if transfer:
+    c.appendleft(table[transfer])
+
+print(c)
