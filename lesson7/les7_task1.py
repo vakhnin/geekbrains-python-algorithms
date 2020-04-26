f9cbf3b1da321c65c2243f08b32721fff9d522c6
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-04-26 13:09:05 +0300

    Задача 1: доработка функции проверки

--- lesson7/les7_task1.py
+++ lesson7/les7_task1.py
@@ -4,6 +4,7 @@
 #  алгоритм сортировки должен быть в виде функции, которая принимает на вход массив данных,
 #  постарайтесь сделать алгоритм умнее, но помните, что у вас должна остаться сортировка пузырьком.
 import random
+import operator
 
 SIZE = 10
 MIN_ITEM = -100
@@ -12,10 +13,20 @@ MAX_ITEM = 100 - 1
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 
 
-def test_sort(arr):
+def test_sort(arr, compare):
     for i in range(len(arr) - 1):
-        assert arr[i] >= arr[i + 1]
-    print('Массив отсортирован по убыванию')
+        if compare == "desc":
+            assert arr[i] >= arr[i + 1]
+        elif compare == "asc":
+            assert arr[i] <= arr[i + 1]
+        else:
+            assert False
+    if compare == "asc":
+        print('Массив отсортирован по возрастанию')
+    elif compare == "desc":
+        print('Массив отсортирован по убыванию')
+    else:
+        print('Неизвестно, как отсортирован массив')
 
 
 def bubble_sort(arr):
@@ -35,6 +46,6 @@ def bubble_sort(arr):
 print("Исходный массив")
 print(array)
 bubble_sort(array)
-test_sort(array)
+test_sort(array, "desc")
 print('Массив после сортировки')
 print(array)
